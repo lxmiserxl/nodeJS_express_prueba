aabeec72624a19f8cd5d4dd57dcaa6a693f6d87f
lxmiserxl <55332262+lxmiserxl@users.noreply.github.com>
2023-11-28 16:47:54 -0500

    Update index.js
    dsf

--- index.js
+++ index.js
@@ -11,20 +11,21 @@ const ID_NUMERO_TELEFONO = process.env.ID_NUMERO_TELEFONO
 const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN
 
-function getSignature(buf) {
-    var hmac = crypto.createHmac("sha1", VERIFY_TOKEN);
-    hmac.update(buf, "utf-8");
-    return "sha1=" + hmac.digest("hex");
-  }
-
-function verifyRequest(req, res, buf, encoding) {
-    var expected = req.headers['x-hub-signature'];
-    var calculated = getSignature(buf);
-    console.log("X-Hub-Signature:", expected, "Content:", "-" + buf.toString('utf8') + "-");
-    if (expected !== calculated) {
-      throw new Error("Invalid signature.");
+function verifyRequestSignature(req, res, buf) {
+    var signature = req.headers["x-hub-signature-256"];
+  
+    if (!signature) {
+      console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
     } else {
-      console.log("Valid signature!");
+      var elements = signature.split("=");
+      var signatureHash = elements[1];
+      var expectedHash = crypto
+        .createHmac("sha256", config.appSecret)
+        .update(buf)
+        .digest("hex");
+      if (signatureHash != expectedHash) {
+        throw new Error("Couldn't validate the request signature.");
+      }
     }
   }
 
@@ -42,7 +43,7 @@ app.listen(8000 || process.env.PORT, () => {
 });
 
 
-app.use(body_parser.json({ verify: verifyRequest }))
+app.use(body_parser.json({ verify: verifyRequestSignature }))
 
 // Add an error-handling Express middleware function 
 // to prevent returning sensitive information.
